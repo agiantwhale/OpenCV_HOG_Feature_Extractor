8eba3f9cc3997379f4378e4467d3b29313b7877a
Il Jae Lee <iljae@umich.edu>
2015-09-19 22:56:02 -0400

    Removed C++11 features.

--- svmexport.cpp
+++ svmexport.cpp
@@ -79,9 +79,9 @@ int main(int argc, char** argv) {
   }
 
   std::ofstream result_data;
-  result_data.open(output_file, std::ios::out|std::ios::app);
+  result_data.open(output_file, std::ofstream::out|std::ofstream::app);
 
-  for(auto iter=single_detector_vector.begin(); iter!=single_detector_vector.end(); iter++) {
+  for(std::vector<float>::iterator iter=single_detector_vector.begin(); iter!=single_detector_vector.end(); iter++) {
     result_data << *iter << std::endl;
   }
 
